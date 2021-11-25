44ebbad7d3a05d28403fdee34c99963f6e1d8b85
Storm <79418708+officalcheats@users.noreply.github.com>
2021-11-25 15:54:27 +0100

    Add files via upload

---
+++ WhiteList.lua
@@ -0,0 +1,13 @@
+if getgenv().key == "-6sNh4n5b5y6)9/+" then
+    print("ol")
+
+if game.PlaceId == 13822889 then
+    game.StarterGui:SetCore("SendNotification", {Title = "DogCheats!", Text = "Welcome to DogCheats", Icon = "", Duration = 2.5})
+   
+    else
+        game.StarterGui:SetCore("SendNotification", {Title = "DogCheats! Error", Text = "Not a supported game", Icon = "", Duration = 2.5})
+end
+
+
+  
+    end
